bb241841d69173017b107642fceac3ff999db70f
Prithwi Chanda <prithwichanda1996@gmail.com>
2025-04-20 23:20:46 +0530

    Revert "Home work removed"
    This reverts commit c582000ec9991cdc2b6f79c3bb5bd569347b3cee.

--- index.js
+++ index.js
@@ -30,3 +30,14 @@ app.get("/api/user/:name", middleware, (request, response, next) => {
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+/**
+ * Create a node js server
+ * 1. Create a basic express server
+ * 2. Create a database connection to it.
+ * 3. Create a Post API that will store data in a table in the database.
+ * 4. Create a Get API that will fetch all data from the table.
+ * 5. Create a Get API that will fetch data from the database with a parameter.
+ * 6. Create a PUT API that will update data in the database.
+ * 7. Create a DELETE API that will delete data from the database.
+ */
